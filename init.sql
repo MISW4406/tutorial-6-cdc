fdf7e51ae9bac8f67947d9556860ab4271efa8df
Juan Urrego <juancho088@gmail.com>
2023-01-26 13:21:24 +0000

    Additional config

---
+++ init.sql
@@ -0,0 +1,5 @@
+CREATE TABLE new_table(  
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
+    create_time DATETIME COMMENT 'Create Time',
+    name VARCHAR(255)
+) COMMENT '';
