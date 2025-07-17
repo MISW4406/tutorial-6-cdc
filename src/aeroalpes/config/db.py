fad579af2119adbf9e6783a494c5236330c83b6a
Jaime Andrés Boyacá Silva <j.boyaca@uniandes.edu.co>
2025-07-17 18:36:27 -0500

    Remove SQLite fallback and revert protobuf

--- src/aeroalpes/config/db.py
+++ src/aeroalpes/config/db.py
@@ -4,10 +4,9 @@ import os
 
 db = None
 
-DB_USERNAME = os.getenv('DB_USERNAME', default='root')
-DB_PASSWORD = os.getenv('DB_PASSWORD', default='adminadmin')
-DB_HOSTNAME = os.getenv('DB_HOSTNAME', default='localhost')
-DB_ENGINE = os.getenv('DB_ENGINE', default='mysql')
+DB_USERNAME = os.getenv('DB_USERNAME', default="root")
+DB_PASSWORD = os.getenv('DB_PASSWORD', default="adminadmin")
+DB_HOSTNAME = os.getenv('DB_HOSTNAME', default="localhost")
 
 class DatabaseConfigException(Exception):
     def __init__(self, message='Configuration file is Null or malformed'):
@@ -18,8 +17,8 @@ class DatabaseConfigException(Exception):
 def database_connection(config, basedir=os.path.abspath(os.path.dirname(__file__))) -> str:
     if not isinstance(config,dict):
         raise DatabaseConfigException
-
-    if DB_ENGINE == 'sqlite' or config.get('TESTING', False) == True:
+    
+    if config.get('TESTING', False) == True:
         return f'sqlite:///{os.path.join(basedir, "database.db")}'
     else:
         return f'mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOSTNAME}/reservas'
